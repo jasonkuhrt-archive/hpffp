cea50d4ae29257da4b0b31389a8c329826ef792f
Jason Kuhrt <jasonkuhrt@me.com>
2016-10-23 14:28:44 -0400

    1.1 Notes

---
+++ 1-all-you-need-is-lambda/Main.hs
@@ -0,0 +1,23 @@
+
+main :: IO ()
+main = undefined
+
+{-
+# Chapter 1 – All You Need is Lambda
+
+
+
+## 1.1 All you need is lambda
+
+* "lambda calculus" (LC)
+* a model of computation
+* developed in 1930s
+* By Alonzo Church
+
+* "calculus"
+* a method of calculation or reasoning
+
+* LC formalizes the concept of effective computability
+* means it determines which problems (or classes of problems) can be solved via computation.
+* Haskell is built on LC
+-}
