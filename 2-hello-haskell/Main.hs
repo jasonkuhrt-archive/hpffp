34c0f29ec29038661a6954181f4da43e606579fb
Jason Kuhrt <jasonkuhrt@me.com>
2016-10-25 20:58:20 -0400

    2.2 Notes

--- 2-hello-haskell/Main.hs
+++ 2-hello-haskell/Main.hs
@@ -8,8 +8,34 @@ main = undefined
 
 ## 2.1 Hello, Haskell
 
-* Use stack to install haskell, haskellstack.org
+* use stack to install haskell, haskellstack.org
 * includes ghci (repl), ghc (haskell), and build tool
 * brew install haskell-stack
 * another guide to getting up and running with haskell is available at https://github.com/bitemyapp/learnhaskell
+
+
+
+## 2.2 Interacting with Haskell code
+
+* two ways of working with code
+* typing directly into ghci, a repl (read-eval-print-loop)
+* loading a source file into ghci
+* repl originated with lisp
+* `ghci` command available if haskell installed standalone
+* otherwise must use `stack ghci`
+* Prelude is a library of standard functions
+* automatically imported
+* can be disabled e.g. for an alternative
+* Prelude library is within package `base`
+* ghci has special commands prefixed by `:` like :info :quit :type
+-}
+
+sayHello :: String -> IO ()
+sayHello x = putStrLn ("Hello, " ++ x ++ "!")
+
+{-
+* `::` denotes a type
+* can be read as "... has the type of ..."
+* in ghci use :load to load a file, path is relative to `pwd` 
+* in ghci use :module to unload the loaded module
 -}
