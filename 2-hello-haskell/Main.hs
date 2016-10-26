6ff8f2581c209f450e06e6544717e7ed324a7c01
Jason Kuhrt <jasonkuhrt@me.com>
2016-10-25 20:34:53 -0400

    2.1 Notes

---
+++ 2-hello-haskell/Main.hs
@@ -0,0 +1,15 @@
+main :: IO ()
+main = undefined
+
+{-
+# Chapter 2 – Hello, Haskell!
+
+
+
+## 2.1 Hello, Haskell
+
+* Use stack to install haskell, haskellstack.org
+* includes ghci (repl), ghc (haskell), and build tool
+* brew install haskell-stack
+* another guide to getting up and running with haskell is available at https://github.com/bitemyapp/learnhaskell
+-}
