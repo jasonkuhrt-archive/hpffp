0315344c078d3b50744bdee9bf413c56a36eb9d7
Jason Kuhrt <jasonkuhrt@me.com>
2016-11-05 20:45:21 -0400

    3.1 Notes

---
+++ 3-strings/Main.hs
@@ -0,0 +1,12 @@
+main :: IO ()
+main = undefined
+{-
+
+## 3.1 Printing strings
+
+* "string"
+* data structure used to contain text
+* most computer langauges have this type/terminology
+* usually represented as sequences (AKA lists) of characters
+* in Haskell, a list of characters
+-}
