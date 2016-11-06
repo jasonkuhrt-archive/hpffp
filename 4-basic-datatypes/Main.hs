53ac4496f11dd9944df460971a88a34388d4f3fd
Jason Kuhrt <jasonkuhrt@me.com>
2016-11-06 06:48:38 -0500

    4.2 Notes

--- 4-basic-datatypes/Main.hs
+++ 4-basic-datatypes/Main.hs
@@ -16,4 +16,17 @@ module Chapter4 where
   * type constructors
   * data types
   * data constructors
+
+
+
+## 4.2 What Are Types?
+
+* evaluated expressions reduce to values (AKA redexes)
+* every value has a type
+* types are used to group values that have something in common (AKA types are classifications, categories, groups, ..., of values)
+* "something in common" may be an abstract idea, or specific model to a particular domain
+* thinking about types like mathematical sets will help guide intuition on how types work in a mathematical sense
+  * set theory is the study of mathematical collections of objects
+  * set theory was a precursor to type theory
+  * set theory ideas like disjunction (or), conjunction (and) used to manipulate sets have equivalents in Haskell type system
 -}
