e0827378814e4795df8222e595f765a37f50d9ea
Jason Kuhrt <jasonkuhrt@me.com>
2016-11-06 06:40:25 -0500

    4.1 Notes

--- 4-basic-datatypes/Main.hs
+++ 4-basic-datatypes/Main.hs
@@ -1,4 +1,19 @@
 module Chapter4 where
 
 {-
+
+## 4.1 Basic Datatypes
+
+* Haskell has a robust type system
+* types help with:
+  * readability
+  * safety
+  * maintainability – classified data is easier to systematically restructure later
+* types AKA datatypes
+* types permit solving problems with _less_ code
+* concepts include:
+  * typeclasses
+  * type constructors
+  * data types
+  * data constructors
 -}
