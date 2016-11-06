51a0336f7036374c70a656f9ce6f7ecb95c7b5c0
Jason Kuhrt <jasonkuhrt@me.com>
2016-11-05 20:45:42 -0400

    3.2 Notes

--- 3-strings/Main.hs
+++ 3-strings/Main.hs
@@ -9,4 +9,28 @@ main = undefined
 * most computer langauges have this type/terminology
 * usually represented as sequences (AKA lists) of characters
 * in Haskell, a list of characters
+
+
+
+## 3.2 A First Look at Types
+
+* "types"
+* a way of categorizing values
+* e.g. integers, fractions, booleans, Char, String
+
+* ghci `:type` will return type info for an expression
+
+* "type signature"
+* line of code that defines the types for a function or expresion
+
+* single quote is character
+* like C, .Net
+
+* double quote is string
+* like Clojure, JavaScript (though there single works too)
+
+* "type alias" (AKA type synonym)
+* a type name that has a different name underneath
+* usually used for convenience
+* String is another name for [Char]
 -}
